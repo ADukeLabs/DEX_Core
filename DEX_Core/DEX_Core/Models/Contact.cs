98eea1e903a0ee7273058a444b2f3624bec44134
ADukeLabs <d.alex.duke@gmail.com>
2019-07-14 20:54:55 +1200

    Domain Models for City, Company, and Contact created.

---
+++ DEX_Core/DEX_Core/Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DEX_Core_WebAPI.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public Company Company { get; set; }
+    }
+}
