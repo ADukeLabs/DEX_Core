0388010059a314e7bbf931cc87252cca2e684f4e
ADukeLabs <d.alex.duke@gmail.com>
2019-07-20 11:21:02 +1200

    EF Core DBContext Created
    DbContext for code first migrations with Entity Framework Core created.

---
+++ DEX_Core/DEX_Core/Migrations/Dex_CoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DEX_Core_WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DEX_Core_WebAPI.Migrations
+{
+    public class Dex_CoreDbContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Contact> Contact { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Intitial Catalog=DexCoreDB");
+        }
+    }                      
+}
